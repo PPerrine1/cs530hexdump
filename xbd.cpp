11d6f9f4e02c9715d1d16dafd86d1c3476bda97b
Patrick <perrinepatrick@gmail.com>
2019-02-13 15:56:43 -0800

    added check loop that checks if the file contains any ASCII content and accounts for a single newline, commented out second loop for further development

--- xbd.cpp
+++ xbd.cpp
@@ -17,25 +17,66 @@ void printHexNibble(char* str) {
 int main(int argc, char* argv[]){
     string input;
     string lineAscii;
-    string addr = "0000000: ";
-    bool binary = false;
+    int addr = 0x00;
+    int lineChk = 0;
+    char cmp;
+    bool isASCII = false;
+    bool ifNewLine = false;
+    bool outBinary = false;
+    ifstream fileCheck;
+    ifstream fileCon;
     
     if(argc == 3)
-        binary = true;
+        outBinary = true;
     
-    ifstream file(argv[(1+(argc-2))]);
+    fileCheck.open(argv[(1+(argc-2))]);
     
-    while(file.is_open() && file.good() && !file.eof()) {
-        getline(file, input);
-        if(!binary) {
+    while(fileCheck.is_open() && fileCheck.good() && !fileCheck.eof()) {
+        getline(fileCheck, input);
+        for(int i=0; i<input.length(); i++) {
+            cmp = input[i];
+            if(cmp != '0' && cmp != '1') {
+                if(cmp == '\n') {
+                    if(ifNewLine) {
+                        isASCII = true;
+                        printf("found extra newline\n");
+                        fileCheck.close();
+                        break;
+                    }
+                    else
+                        ifNewLine = true;
+                }
+                else {
+                    isASCII = true;
+                    printf("found ASCII char at %d\n", i);
+                    fileCheck.close();
+                    break;
+                }
+            }
+            printf("checked character\n");
+        }
+    }
+    
+    fileCon.open(argv[(1+(argc-2))]);
+    
+    while(fileCon.is_open() && fileCon.good() && !fileCon.eof()) {
+        getline(fileCon, input);
+        /*if(isASCII) {
+            cout << addr << ": ";
             for(int i=0; i<input.length(); i++) {
                 printHexNibble(&input[i]);
+                i++;
+                printHexNibble(&input[i]);
+                cout << " ";
             }
+            addr += 16;
+            cout << endl;
         }
         else {
             
         }
-    
+    */
+        printf("sup");
     }
     
 }
