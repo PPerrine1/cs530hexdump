dbe7ec5ad98e6a881b5780818d98006b53f6eca4
Patrick <perrinepatrick@gmail.com>
2019-02-18 14:39:43 -0800

    hex dump fully working with .txt files, edited test.txt

--- xbd.cpp
+++ xbd.cpp
@@ -3,32 +3,111 @@
 #include <cstring>
 #include <sstream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
-void printHexNibble(char* str) {
-    stringstream ss;
-    ss << hex << unsigned(*str);
-    
-    unsigned res;
-    ss >> res;
-    cout << hex << res;
+bool isPrintable(char c) {
+    int val = (int) c;
+    if(val > 32 && val < 127)
+        return true;
+    else
+        return false;
+}
+
+bool printValidHexNibble(char* str) {
+    if(isPrintable(*str)) {
+        stringstream ss;
+        ss << hex << unsigned(*str);
+            
+        unsigned res;
+        ss >> res;
+            
+        cout << hex << res;
+        return true;
+    }
+    else {
+        if((int)*str < 16)
+            cout << "0";
+        cout << hex << (int)*str;
+        return false;
+    }
+}
+
+std::string addressFormat(int ad)
+{
+  std::stringstream stream;
+  stream << std::setfill('0') << std::setw((sizeof(int)*2)-1) 
+         << std::hex << ad;
+  return stream.str();
 }
 
 int main(int argc, char* argv[]){
-    string input;
-    string lineAscii;
+    char input;
+    string lineAscii = "";
     int addr = 0x00;
-    int lineChk = 0;
-    char cmp;
-    bool isASCII = false;
-    bool ifNewLine = false;
+    
+    bool ifFirstLine = true;
     bool outBinary = false;
-    ifstream fileCheck;
+    
     ifstream fileCon;
     
     if(argc == 3)
         outBinary = true;
     
+    fileCon.open(argv[(1+(argc-2))]);
+    
+    if(!outBinary) {
+        while(fileCon.is_open() && fileCon.good()) {
+            fileCon.get(input);
+            if(fileCon.eof())
+                break;
+            else {
+                if(ifFirstLine) {
+                    cout << addressFormat(addr) << ": ";
+                    ifFirstLine = false;
+                }
+                else if(addr % 16 == 0) {
+                    cout << lineAscii << endl << addressFormat(addr) << ": ";
+                    lineAscii = "";
+                }
+                
+                if(printValidHexNibble(&input))
+                    lineAscii += input;
+                else
+                    lineAscii += ".";
+                
+                addr += 1;
+                
+                if(addr % 2 == 0)
+                    cout << " ";
+            }
+        }
+    }
+    
+    fileCon.close();
+
+    if(addr % 16 != 0) {
+        while(addr % 16 != 0) {
+            if(addr % 2 != 0) {
+                cout << "   ";
+                addr += 1;
+            }
+            else {
+                cout << "     ";
+                addr += 2;
+            }
+        }
+        cout << lineAscii << endl;
+    }
+}
+
+
+//char cmp;
+//bool isASCII = false;
+//ifstream fileCheck;
+
+/* Checked if file contains only ASCII characters or not, found to be useless.
+
     fileCheck.open(argv[(1+(argc-2))]);
     
     while(fileCheck.is_open() && fileCheck.good() && !fileCheck.eof()) {
@@ -56,27 +135,4 @@ int main(int argc, char* argv[]){
             printf("checked character\n");
         }
     }
-    
-    fileCon.open(argv[(1+(argc-2))]);
-    
-    while(fileCon.is_open() && fileCon.good() && !fileCon.eof()) {
-        getline(fileCon, input);
-        /*if(isASCII) {
-            cout << addr << ": ";
-            for(int i=0; i<input.length(); i++) {
-                printHexNibble(&input[i]);
-                i++;
-                printHexNibble(&input[i]);
-                cout << " ";
-            }
-            addr += 16;
-            cout << endl;
-        }
-        else {
-            
-        }
-    */
-        printf("sup");
-    }
-    
-}
+*/
