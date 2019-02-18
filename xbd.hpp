d0ab2e622682ced6efe9c5a92b93a7332a568d7c
Generic Account 0448 <cssc0448@edoras.sdsu.edu>
2019-02-18 13:59:25 -0800

    Added include file skeleton

---
+++ xbd.hpp
@@ -0,0 +1,16 @@
+/**
+ * Name: Rhythm Mecwan (cssc0448) & Patrick Perine
+ * Project: CS530 Assignment 1
+ * File: xbd.hpp
+ * Notes: Header file with all data types, declarations, and macro definitions.
+ */
+
+
+
+
+
+#include <iostream>
+
+
+
+/**************************[ EOF: xbd.hpp ]************************/
